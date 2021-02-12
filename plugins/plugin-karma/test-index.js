cc9043e29459158ca6ba8f3171cee72e6f4d44ea
Justin Spears <speajus@gmail.com>
2021-02-12 12:05:18 -0800

    fixes testIndex resolution

--- plugins/plugin-karma/test-index.js
+++ plugins/plugin-karma/test-index.js
@@ -1,13 +1,13 @@
+console.log('running test-index');
+
+window.test = it;
+
 describe(
     `started '${MRBUILDER_TEST_MODULE}'`,
     () => it('😀', () => {
     }));
 console.log(MRBUILDER_TEST_MODULE);
-(tc => tc.keys().forEach(key => {
-    describe(key, function () {
-        tc(key);
-    });
-}))(require.context("@mrbuilder/karma-test-context"));
+(tc => tc.keys().forEach(key => describe(key, ()=>tc(key))))(require.context("@mrbuilder/karma-test-context"));
 
 describe(`finished '${MRBUILDER_TEST_MODULE}'`, function () {
     it('🙄', () => {
